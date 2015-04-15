8e938505a4939c993bd7a629cff6892d89ec1ab1
DanielJCampbell <campbelldj@hotmail.com>
2015-04-15 15:51:26 +1200

    Updated Agent.js with test code

--- IAScript/Agent.js
+++ IAScript/Agent.js
@@ -3,32 +3,38 @@ function Agent() {
   var data = require("./Data.js");
   this.User = dataTypes.User;
   this.Post = dataTypes.Post;
-  this.Sub = dataTypes.Subreddit;
   this.Script = require("./script.js");
   this.bot = new Script();
   this.users = [];
   this.posts = [];
-  this.subs = [];
 }
 
 Agent.prototype.init = function(subreddit) {
   //All asynchronous with callbacks
   this.bot.reddit.auth().then(buildPosts(subreddit))
-  .then(buildUsers()).then(reRank()).then(function() {
+  .then(reRank()).then(function() {
     this.bot.reddit.deauth();
   }).done();
 };
 
 Agent.prototype.buildPosts = function(subreddit) {
-  this.bot.reddit()
-};
-
-Agent.prototype.buildUsers = function() {
-
+  this.bot.reddit("/r/$subreddit/new").listing({
+    $subreddit = subreddit,
+    limit : 50
+  }).then(function(slice) {
+    slice.children.forEach(function(item, i) {
+      console.log("Item #" + i);
+      for (prop in item) {
+        if (item.hasOwnProperty(prop))
+          console.log("\t" + prop);
+      }
+    })}).done();
 };
 
 Agent.prototype.reRank = function() {
 
 };
+
+init("funny");
 
 module.exports = Agent;
